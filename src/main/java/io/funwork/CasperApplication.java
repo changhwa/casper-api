f4c47b45cd5eb352e1fa68ca793e2218f728b6f9
Changhwa Oh <changhwaoh.co@gmail.com>
2016-12-26 21:02:41 +0900

    Init Project

---
+++ src/main/java/io/funwork/CasperApplication.java
@@ -0,0 +1,12 @@
+package io.funwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasperApplication.class, args);
+	}
+}
